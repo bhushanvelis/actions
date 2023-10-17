5d36ebd5a005a2c1769c2e1833929540c67f016a
Bhushan <bhushanvelis@gmail.com>
2023-10-17 07:45:55 +0530

    Update writeJson.py

--- testAction/writeJson.py
+++ testAction/writeJson.py
@@ -11,18 +11,18 @@ print(f" >> {arguments.github_repository}")
 print(f">> {parser_github.title}")
 print(f">> {parser}")
 
-    '''
-    # Data to be written
-    dictionary = {
-        "name": "Bhushan",
-        "rollno": 56,
-        "phonenumber": "500"
-    }
+'''
+# Data to be written
+dictionary = {
+"name": "Bhushan",
+"rollno": 56,
+"phonenumber": "500"
+}
      
-    # Serializing json
-    json_object = json.dumps(dictionary, indent=4)
+# Serializing json
+json_object = json.dumps(dictionary, indent=4)
      
-    # Writing to sample.json
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
-    '''
+# Writing to sample.json
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
+'''
