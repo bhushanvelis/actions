9aaf4014e42deea4a46312c47afaa257974b7ea6
bhushan <bhushanvelis@gmail.com>
2023-10-17 06:51:58 +0530

    python added

---
+++ testAction/writeJson.py
@@ -0,0 +1,31 @@
+import json
+import argparse
+parser = argparse.ArgumentParser(__name__)
+
+parser_github = parser.add_argument_group("github")
+parser_github.add_argument("-gt", "--github-token")
+parser_github.add_argument("-gr", "--github-repository")
+
+
+if __name__ == "__main__":
+    arguments = parser.parse_args()
+
+    print(f" >> {arguments.github_repository}")
+    print(f">> {parser_github.title}")
+    print(f">> {parser}")
+
+    '''
+    # Data to be written
+    dictionary = {
+        "name": "Bhushan",
+        "rollno": 56,
+        "phonenumber": "500"
+    }
+     
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
+     
+    # Writing to sample.json
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
+    '''
